ccbd6d81bef6f802b2f7a344933a7b6bcd6d82c6
StephenVP <pham.vu@outlook.com>
2022-09-16 02:54:27 +0700

    Using global variable for static assets

--- frontend/components/static/static.jsx
+++ frontend/components/static/static.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 export default function Static(props){
-    useEffect(()=>{
-        props.fetchImages()
-    },[])
+    // useEffect(()=>{
+    //     props.fetchImages()
+    // },[])
     return(
         props.images ? 
     (
     <div id="slide1" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">
             <div className="carousel-item active">
-                <img src={props.images[0]} className="d-block w-100 opacity-75" alt="..."/>
+                <img src={window.run1Url} className="d-block w-100 opacity-75" alt="..."/>
                     <div className="carousel-caption d-none d-md-block">
                         <h5 className="h1 shadow">Create Your Own Route!</h5>
                         <p className="shadow">You can design your own route to Run</p>
@@ -22,7 +22,7 @@ export default function Static(props){
             </div>
 
             <div className="carousel-item">
-                <img src={props.images[1]} className="d-block w-100 opacity-75" alt="..."/>
+                <img src={window.run2Url} className="d-block w-100 opacity-75" alt="..."/>
                 <div className="carousel-caption d-none d-md-block bg-secondary opacity-80">
                         <h5 className="h1 shadow opacity-100">Record Your Activity!</h5>
                         <p className="shadow opacity-100">Let's the World Know Your Great Achievement!!! </p>
@@ -32,7 +32,7 @@ export default function Static(props){
                 </div>
             </div>
             <div className="carousel-item">
-                <img src={props.images[2]} className="d-block w-100 opacity-75" alt="..."/>
+                <img src={window.run3Url} className="d-block w-100 opacity-75" alt="..."/>
                 <div className="carousel-caption d-none d-md-block">
                         <h5 className="h1 shadow">Be Competitive with Your Friends</h5>
                         <p className="shadow">Let's Find out the Activities of your Friends </p>
