266fc32aa893d3d468461c09a1c169aaa9ad56aa
StephenVP <pham.vu@outlook.com>
2022-09-16 01:45:02 +0700

    Add cache to index static page

--- app/controllers/api/statics_controller.rb
+++ app/controllers/api/statics_controller.rb
@@ -1,6 +1,8 @@
 class Api::StaticsController < ApplicationController
     #use to fetch all the picture
+    
     def index
+        expires_in 12.hours, :public => true
         @images = ["run1","run2","run3"]
         render :index
     end
