c5cc0c511919968d6c630828c34881cc5dc1e184
StephenVP <pham.vu@outlook.com>
2022-09-17 22:34:35 +0700

    Fix cloud storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ config.static_cache_control = "public, max-age=31536000"
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :amazon_prod
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
