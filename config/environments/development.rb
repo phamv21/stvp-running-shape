c96d2f54dc895067382007ca1daf855f5997fb08
StephenVP <pham.vu@outlook.com>
2022-09-17 21:26:26 +0700

    Add AWS bucket for prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   #precompile
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # Do not eager load code on boot.
   config.eager_load = false
